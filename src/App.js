532183987f6f5d7a0667d17c185d777cce832595
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2022-08-30 01:46:56 +0600

    authcontext create

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import Posts from './components/pages/Posts';
 import SignIn from './components/pages/SignIn';
 import SignUp from './components/pages/SignUp';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from './contexts/AuthContext';
 
 
 function App() {
   return (
     <>
       <Router>
-        <Layout>
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route exact path="posts" element={<Posts />} />
-              <Route exact path="signin" element={<SignIn />} />
-              <Route exact path="signup" element={<SignUp />} />
-          </Routes>
-        </Layout>
+        <AuthProvider>
+          <Layout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route exact path="posts" element={<Posts />} />
+                <Route exact path="signin" element={<SignIn />} />
+                <Route exact path="signup" element={<SignUp />} />
+            </Routes>
+          </Layout>
+        </AuthProvider>
       </Router>
     </>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Initialize Firebase
+const app = initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  appId: process.env.REACT_APP_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URl,
+});
 export default app;
