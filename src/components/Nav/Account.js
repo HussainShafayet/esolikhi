f99c3d5265d3b31e4eddf596b3b5f90bacb89a9d
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2022-09-04 00:59:32 +0600

    registration done with login

--- src/components/Nav/Account.js
+++ src/components/Nav/Account.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
 
 export default function Account() {
+  const { currentUser,logout } = useAuth();
+  const username = currentUser ? currentUser.displayName : "";
   return (
     <>
       {/* <Nav className="me-auto my-2 my-lg-0">
@@ -13,12 +15,23 @@ export default function Account() {
           SignUp
         </NavLink>
       </Nav> */}
+      {!currentUser ?
+      <>
       <NavLink className="nav-link" to="signin">
         SignIn
       </NavLink>
       <NavLink className="nav-link" to="signup">
         SignUp
+      </NavLink> </> :
+      <>
+      <NavLink className="nav-link" to="user">
+        {username}
       </NavLink>
+      <NavLink className="nav-link" to="logout" onClick={logout}>
+        Logout
+      </NavLink>
+      </>
+      }
     </>
   );
 }
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,17 +1,55 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import  signupStyle  from '../../assets/css/Nav/signup.module.css'
 import { Link } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
   const [loading, setLoading] = useState(false);
   const [error,setError] = useState(null);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState(''); 
+  const [check, setCheck] = useState(false);
   const { signup } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSignup = (e)=>{
+    e.preventDefault();
+    console.log(username, email,check, password);
+    try{
+      if(password === confirmPassword){
+        if(check){
+          setError('');
+          setLoading(true)
+          signup(email, password, username)
+          setLoading(false)
+          navigate('/');
+          
+        }else{
+          setError('Your must check terms and conditions.')
+          return
+        }
+      }else{
+        setError('Password not matching!')
+        return
+      }
+    }catch(error){
+      console.log('registraion error')
+    }
+  }
   return (
     <>
       <div className={signupStyle.signupForm}>
-        <Form method="post" className={signupStyle.form}>
+        <Form
+          method="post"
+          className={signupStyle.form}
+          onSubmit={handleSignup}
+        >
           <h2>Sign Up</h2>
           <p className={signupStyle.hintText}>
             Create your account. It's free and only takes a minute.
@@ -24,6 +62,7 @@ export default function SignUp() {
                   name="first_name"
                   placeholder="First Name"
                   required="required"
+                  onInput={(e) => setFirstName(e.target.value)}
                 />
               </div>
               <div className="col-sm-6">
@@ -32,9 +71,19 @@ export default function SignUp() {
                   name="last_name"
                   placeholder="Last Name"
                   required="required"
+                  onInput={(e) => setLastName(e.target.value)}
                 />
               </div>
             </div>
+          </Form.Group>
+          <Form.Group className="mb-2">
+            <Form.Control
+              type="text"
+              name="username"
+              placeholder="Username"
+              required="required"
+              onInput={(e) => setUsername(e.target.value)}
+            />
           </Form.Group>
           <Form.Group className="mb-2">
             <Form.Control
@@ -42,6 +91,7 @@ export default function SignUp() {
               name="email"
               placeholder="Email"
               required="required"
+              onInput={(e) => setEmail(e.target.value)}
             />
           </Form.Group>
           <Form.Group className="mb-2">
@@ -50,6 +100,7 @@ export default function SignUp() {
               name="password"
               placeholder="Password"
               required="required"
+              onInput={(e) => setPassword(e.target.value)}
             />
           </Form.Group>
           <Form.Group className="mb-3">
@@ -58,22 +109,29 @@ export default function SignUp() {
               name="confirm_password"
               placeholder="Confirm Password"
               required="required"
+              onInput={(e) => setConfirmPassword(e.target.value)}
             />
           </Form.Group>
           <Form.Group className="form-check d-flex align-items-center mb-5">
-            <Form.Control className="form-check-input me-2" type="checkbox" />
+            <Form.Control className="form-check-input me-2" type="checkbox" onInput={(e)=>setCheck(e.target.checked)} />
             <Form.Label className="mb-0">
-              I agree all statements in <Link className='d-inline' to=''>Terms of service</Link>
+              I agree all statements in{" "}
+              <Link className="d-inline" to="">
+                Terms of service
+              </Link>
             </Form.Label>
           </Form.Group>
-          <Form.Group className='text-center'>
+          <Form.Group className="text-center">
             <Button type="submit" className="btn btn-success btn-block">
               SignUp Now
             </Button>
           </Form.Group>
           <div className="text-center">
-          Already have an account? <Link className='d-inline' to="/signin">Sign in</Link>
-        </div>
+            Already have an account?{" "}
+            <Link className="d-inline" to="/signin">
+              Sign in
+            </Link>
+          </div>
         </Form>
       </div>
     </>
