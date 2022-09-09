dc57314bcbcf0e4377e1a80eba93746852d43722
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2022-09-09 15:42:44 +0600

    post form added and connect api

--- src/components/pages/Posts.js
+++ src/components/pages/Posts.js
@@ -1,7 +1,60 @@
 import React from 'react'
+import { useState } from 'react';
+import { Form,Button } from 'react-bootstrap';
+import { getDatabase, ref, set,push, get } from "firebase/database";
+import { useAuth } from '../../contexts/AuthContext';
+import { useEffect } from 'react';
+
 
 export default function Posts() {
+  const [title, setTitle] = useState();
+  const [post, setPost] = useState();
+  const { currentUser } = useAuth();
+
+  const handlePost = async (e)=>{
+    e.preventDefault();
+    let url = "http://127.0.0.1:8000/createorgetpost";
+    let postInfo = {
+      title: title,
+      details: post,
+    };
+   const requestOptions = {
+     method: "POST",
+     headers: { "Content-Type": "application/json" },
+     body: JSON.stringify(postInfo),
+   };
+    fetch(url,
+      requestOptions
+    )
+      .then((response) => console.log('res',response.json()))
+      .then((data) => console.log('data',data));
+  }
+  
   return (
-    <div>Posts</div>
-  )
+    <>
+      <Form action="post" onSubmit={handlePost}>
+        <Form.Group>
+          <Form.Control
+            type="text"
+            name="title"
+            placeholder="Title"
+            onInput={(e) => setTitle(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            type="text"
+            name="post"
+            placeholder="Write something"
+            onInput={(e) => setPost(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Button type="submit" className="btn btn-sm">
+            Post
+          </Button>
+        </Form.Group>
+      </Form>
+    </>
+  );
 }
