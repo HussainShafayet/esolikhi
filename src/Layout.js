2e839942741907018d0ede494eb41d962dcf948c
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2022-10-04 01:29:56 +0600

    layout responsive

--- src/Layout.js
+++ src/Layout.js
@@ -4,13 +4,47 @@ import NavBar from './components/Nav/NavBar'
 export default function Layout({children}) {
   return (
     <>
-        <NavBar />
+      {/* <NavBar />
         <main>
             <div className='container'>
                  {children}
             </div>
-        </main>
-        {/* <Footer /> */}
+        </main> */}
+      {/* <Footer /> */}
+      <NavBar />
+      <main>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 col-sm-12 col-xs-12 bg-primary p-1">
+              header
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-3 col-xs-12 col-sm-3 pl-0 pr-0">
+              <div className="col-md-12 col-sm-12 col-xs-12 bg-secondary p-1 mt-1">
+                2
+              </div>
+              <div className="col-md-12 col-sm-12 col-xs-12 bg-info p-1 mt-1">
+                3
+              </div>
+              <div className="col-md-12 col-sm-12 col-xs-12 bg-danger p-1 mt-1">
+                4
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-9 col-xs-12 p-1 mid-content bg-success">
+              {children}
+            </div>
+            <div className="col-md-3 col-sm-12 col-xs-12 bg-light p-1 height mt-1">
+              6
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12 col-sm-12 col-xs-12 bg-warning p-1 mt-1">
+              <footer>footer</footer>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
