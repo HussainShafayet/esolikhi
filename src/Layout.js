4d4dd65e2588f077c04cbfbe68b3f6b30057d253
HussainShafayet <shafayetsec09@gmail.com>
2024-03-28 03:20:42 +0600

    grid layout use

--- src/Layout.js
+++ src/Layout.js
@@ -1,43 +1,31 @@
-import React from 'react'
-import NavBar from './components/Nav/NavBar'
+import React from 'react';
+import NavBar from './components/Nav/NavBar';
+import style from './Layout.module.css'
 
 export default function Layout({children}) {
   return (
     <>
-      <NavBar />
-      <main>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12 col-sm-12 col-xs-12 bg-primary p-1">
-              header
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-3 col-xs-12 col-sm-3 pl-0 pr-0">
-              <div className="col-md-12 col-sm-12 col-xs-12 bg-secondary p-1 mt-1">
-                2
-              </div>
-              <div className="col-md-12 col-sm-12 col-xs-12 bg-info p-1 mt-1">
-                3
-              </div>
-              <div className="col-md-12 col-sm-12 col-xs-12 bg-danger p-1 mt-1">
-                4
-              </div>
-            </div>
-            <div className="col-md-6 col-sm-9 col-xs-12 p-1 mid-content vh-100">
-              {children}
-            </div>
-            <div className="col-md-3 col-sm-12 col-xs-12 bg-light p-1 height mt-1">
-              Recent Post.....
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12 col-sm-12 col-xs-12 bg-warning p-1 mt-1">
-              <footer>footer</footer>
-            </div>
-          </div>
-        </div>
+    <div className={style.blogLayout}>
+      <header  className={style.header}>
+          <NavBar />
+      </header>
+      <nav  className={style.nav}>
+        navigation
+      </nav>
+
+      <main  className={style.main}>
+        {children}
       </main>
+
+      <footer  className={style.footer}>
+        footer
+      </footer>
+
+      <div  className={style.feeds}>
+        feeds
+      </div>
+    </div>
+
     </>
   );
 }
